5d8a47c6d75627ecb696c49fdfb0a9d5106b90eb
Hamza Dincer <hamzamustakdincer@gmail.com>
2024-05-08 08:41:11 -0400

    changed the gamestart to button

--- src/components/BeginScreen/BeginScreen.jsx
+++ src/components/BeginScreen/BeginScreen.jsx
@@ -1,28 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./BeginScreen.scss";
 
 const BeginScreen = ({ onGameStart }) => {
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      if (event.code === "Space") {
-        onGameStart();
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyPress);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyPress);
-    };
-  }, [onGameStart]);
-
   return (
     <div className="begin-screen">
       <h1>Deneme Bitti</h1>
       <p>
-        Deneme turları sona erdi. Gerçek oyun başlayacak. Başlamak için boşluk
-        tuşuna basın.
+        Deneme turları sona erdi. Gerçek oyun başlayacak. Oyunu başlatmak için
+        butona tıklayın.
       </p>
+      <button onClick={onGameStart}>Oyunu Başlat</button>
     </div>
   );
 };
